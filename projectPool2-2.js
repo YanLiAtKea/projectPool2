2a7240eb983e3e5ede18767274f2912c2d7df900
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-20 16:35:53 +0200

    Changed a bit so that no error in the inspector anymore. Added buttons to stop and resume the loop

--- projectPool2-2.js
+++ projectPool2-2.js
@@ -1,23 +1,43 @@
 let imgs = document.querySelectorAll('.img');
 let imgNr = imgs.length;
 let index =  imgNr-1;
+let startB = document.querySelector('.start');
+let stopB = document.querySelector('.stop');
+let runF = setInterval(run, 3000);
 
 window.addEventListener('load', function(){
-    setInterval(run, 3000);
+    run(); // so no need to wait 3s for the RUN to run
+    runF;
 })
+
+stopB.addEventListener('click', stopLoop);
+function stopLoop(){
+    clearInterval(runF);
+    stopB.style.display = "none";
+    startB.style.display = "inherit";
+}
+startB.addEventListener('click', restart);
+function restart(){
+    startB.style.display = "none";
+    stopB.style.display = "inherit";
+    run();
+    runF = setInterval(run, 3000); // have to write this way, so that the second time user click on stop ( ie stop- start- stop ) JS still knows which interval to concel.
+}
 
 
 function run(){
     imgs[index].classList.add('fadeOut');
-    index--;
-    if(index==-1){
+    if (index >= 1){
+        index--;
+    } else {
         imgs[0].addEventListener('animationend', startOver);
-            function startOver(){
-                imgs.forEach(removeFadeOut);
-                function removeFadeOut(img, order){
-                    img.classList.remove('fadeOut');
+        function startOver(){
+            imgs.forEach(removeFadeOut);
+            function removeFadeOut(img, order){
+                img.classList.remove('fadeOut');
             }
             index=imgNr-1;
         }
+
     }
 }
