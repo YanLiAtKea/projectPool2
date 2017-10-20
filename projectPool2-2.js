0f44da2297fe9e6cbbfc78890ba6104e559db726
Yan Li <Yan@Yans-MacBook-Air.local>
2017-10-20 13:46:00 +0200

    we are kinda better than Jonas~~

--- projectPool2-2.js
+++ projectPool2-2.js
@@ -3,7 +3,7 @@ let imgNr = imgs.length;
 let index =  imgNr-1;
 
 window.addEventListener('load', function(){
-    setInterval(run, 1000);
+    setInterval(run, 3000);
 })
 
 
@@ -11,36 +11,13 @@ function run(){
     imgs[index].classList.add('fadeOut');
     index--;
     if(index==-1){
-        imgs.forEach(removeFadeOut);
-            function removeFadeOut(img, order){
-
+        imgs[0].addEventListener('animationend', startOver);
+            function startOver(){
+                imgs.forEach(removeFadeOut);
+                function removeFadeOut(img, order){
                     img.classList.remove('fadeOut');
-
-            }
-        index=imgNr-1
-    }
-}
-/*
-window.onload = function startSwitch(){
-    imgs[index].classList.add('fadeOut');
-    imgs[index].addEventListener('animationend', fadeOutNextOne);
-    function fadeOutNextOne(){
-        if (index >=1){
-            index--;
-            imgs[index].classList.add('fadeOut');
-            imgs[index].addEventListener('animationend', startSwitch());
-
-        } else {
-            console.log(index)
-            imgs.forEach(removeFadeOut);
-            function removeFadeOut(img, order){
-
-                    img.classList.remove('fadeOut');
-
             }
             index=imgNr-1;
-            imgs[index].classList.add('fadeOut');
-
         }
     }
-}*/
+}
